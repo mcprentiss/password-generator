83418e426a43361cc8dce7bec365c8498d0c9637
mp466 <mcprentiss@gmail.com>
2015-02-03 15:27:52 -0600

    improve details of code

--- test_password.py
+++ test_password.py
@@ -6,18 +6,22 @@ from password import main
 import os
 import re
 
+
 def test_password_len():
     """ test length is long enough"""
     assert len(main()) > 18
+
 
 def test_password_num():
     """ test password has a numeral"""
     assert re.search('[0-9]', main())
 
+
 def test_password_letter():
     """ test password has a letter"""
     assert re.search('[A-Za-z]', main())
 
+
 def test_existance_dict_words():
     """ test if /usr/share/dict/words exists"""
-    assert os.path.isfile("/usr/share/dict/words") == True
+    assert os.path.isfile("/usr/share/dict/words") is True
