162eaca4db833db03a606e04e3d2062a4e824783
mp466 <mcprentiss@gmail.com>
2015-04-09 11:19:04 -0500

    new password.py

--- password.py
+++ password.py
@@ -13,11 +13,12 @@ def main():
         print('/usr/share/dict/word will not open')
     sss = [x.strip() for x in fff.readlines()]
     pword = ''.join(random.choice(sss) for i in range(1)).capitalize()
-    lll = list(pword[0:8] + '!@#$%^&*' +
-               str(random.randint(10000000, 99999999)))
+    lll = list(
+        pword[0:8] + '!@#$%^&*' + str(random.randint(10000000, 99999999)))
     random.shuffle(lll)
     result = ''.join(lll)
     return result
+
 
 if __name__ == '__main__':
     print(main())
