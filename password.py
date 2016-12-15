cefb3cc62dba7d1386a966ff00e0583fc318629b
Michael Prentiss <mcprentiss@gmail.com>
2016-12-14 18:51:14 -0600

    misc

--- password.py
+++ password.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" generate random passwords mike"""
+"""Generate random passwords."""
 # -*- coding: utf-8 -*-
 
 import random
@@ -9,15 +9,16 @@ import random
 # @q
 # q(fff)
 
-
 def main():
-    """    main function of password """
+    """ Main function of password."""
     try:
         fff = open('/usr/share/dict/words')
     except IOError:
         print('/usr/share/dict/word will not open')
     sss = [x.strip() for x in fff.readlines()]
-    pword = ''.join(random.choice(sss) for i in range(1)).capitalize()
+    print(random.choice(sss).capitalize())
+    pword = ''.join(random.choice(sss)).capitalize()
+    # pword = ''.join(random.choice(sss) for i in range(1)).capitalize()
     lll = list(pword[0:8] + '!@#$%^&*' + str(random.randint(10000000, 99999999)))
     random.shuffle(lll)
     result = ''.join(lll)
